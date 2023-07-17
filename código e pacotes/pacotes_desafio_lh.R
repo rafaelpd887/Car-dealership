1c88f68c27305bc14250c3915df160b9127fd90b
rafaelpd887 <129010508+rafaelpd887@users.noreply.github.com>
2023-07-17 18:52:11 -0300

    Update pacotes_desafio_lh.R

--- código e pacotes/pacotes_desafio_lh.R
+++ código e pacotes/pacotes_desafio_lh.R
@@ -4,10 +4,6 @@ pacotes <- c("readxl", # permite "ler" arquivos "xlsx"
              "fastDummies", # "dummização" de variáveis qualitativas (One Hot Coding). 
              "ggplot2", # funções para plotagem de gráficos
              "car", # funções para análise estatística
-             "olsrr", # ols_test_breusch_pagan
-             "nortest", # sf_test
-             "openxlsx", # permite salvar em formato "xlsx"
-             "rpart", # árvores
              "caret" , # xgbtree entre outras funções
              "readr"  # permite salvar arquivos "csv"
              )
@@ -23,29 +19,3 @@ if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
 
 
 
-lapply(pacotes, library, character.only = TRUE)
-
-
-saveRDS(modelo_cars2, "modelo_cars.rds")
-saveRDS(bc_modelo_cars, "bc_modelo_cars.rds")
-modelo_cars <- readRDS("modelo_cars.rds")
-
-
-
-
-
-
-# Avaliando o modelo1 (usando modelo e versao e cidade)
-
-summary(modelo_cars) ## R² = 0.7821
-## através do summary do modelo, podemos ver que muitas das variáveis não 
-## possuem significância estatistica se considerarmos um nível de confiança de 95%.
-
-vif(modelo_cars)
-## ao executarmos a função `vif`, retornamos um erro relacionado a alta correlação entre algumas
-## variáveis.
-
-##modelo2 (- modelo,-versao, -cidade) R² = 0.55 (R² stepwise tbm = 0.55)
-##modelo3 (- versao, -cidade) R2 = 0.68
-##MODELO4 (-VERSAO) R2 = 0.70
-##MODELO5 (-NUM_PORTAS) R² = 0.7809
