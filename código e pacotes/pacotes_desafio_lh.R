9fc98b17355d8666da8a226dceeee28c971dfd4f
rafaelpd887 <129010508+rafaelpd887@users.noreply.github.com>
2023-10-07 19:17:06 -0300

    Update pacotes_desafio_lh.R

--- código e pacotes/pacotes_desafio_lh.R
+++ código e pacotes/pacotes_desafio_lh.R
@@ -1,21 +1,22 @@
-pacotes <- c("readxl", # permite "ler" arquivos "xlsx"
-             "dplyr", # funções para análise estatística 
-             "PerformanceAnalytics", # adiciona outras funções para análise, 
-             "fastDummies", # "dummização" de variáveis qualitativas (One Hot Coding). 
-             "ggplot2", # funções para plotagem de gráficos
-             "car", # funções para análise estatística
-             "caret" , # xgbtree entre outras funções
-             "readr"  # permite salvar arquivos "csv"
+packages <- c("readxl", # allows "reading" "xlsx" files
+             "dplyr", # functions for statistical analysis 
+             "PerformanceAnalytics", # adds other functions for analysis, 
+             "fastDummies", # "dummy coding" of qualitative variables (One Hot Coding). 
+             "ggplot2", # functions for plotting graphs
+             "car", # functions for statistical analysis
+             "caret" , # xgbtree among other functions
+             "readr"  # allows saving "csv" files
              )
-if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
-  instalador <- pacotes[!pacotes %in% installed.packages()]
-  for(i in 1:length(instalador)) {
-    install.packages(instalador, dependencies = T)
+if(sum(as.numeric(!packages %in% installed.packages())) != 0){
+  installer <- packages[!packages %in% installed.packages()]
+  for(i in 1:length(installer)) {
+    install.packages(installer, dependencies = T)
     break()}
-  sapply(pacotes, require, character = T) 
+  sapply(packages, require, character = T) 
 } else {
-  sapply(pacotes, require, character = T) 
+  sapply(packages, require, character = T) 
 }
+
 
 
 
